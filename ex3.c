3ec70e6b04054c983020218498dbfc1b3e283ec1
Emily Anna Holdsclaw <emily33@classroom.cs.unc.edu>
2018-05-14 11:13:34 -0400

    First commit

---
+++ ex3.c
@@ -0,0 +1,20 @@
+/*  Example: C program to find area of a circle */
+
+#include <stdio.h>
+#define PI 3.14159
+
+int main()
+{
+  float r, a, b;
+
+while (a != 0) {
+  printf("Enter radius (in cm):\n");
+  scanf("%f", &r);
+
+  a = PI * (r/2.54) * (r/2.54);
+  b = 2 * PI * r / 2.54;
+
+  printf("Circle's area is %3.2f (sq in).\n", a);
+  printf("Its circumference is %3.2f (in).\n", b);
+  }
+}
